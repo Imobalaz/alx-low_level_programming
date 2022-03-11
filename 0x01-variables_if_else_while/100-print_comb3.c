2bdf98df254232c7f525efccd56559994f4a4d94
Imobalaz <imobalaz@gmail.com>
2022-03-11 07:42:14 -0800

    Combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main header
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+	int n;
+
+	for (m = 48; m < 58; m++)
+	{
+
+		for (n = m + 1; n < 58; n++)
+		{
+			if (m != 48 || n != 49)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			putchar(m);
+			putchar(n);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
+
