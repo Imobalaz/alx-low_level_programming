d2abdedbcca463c37a03d8fb9fb57f4957d32d79
Imobalaz <imobalaz@gmail.com>
2022-03-11 07:04:07 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main part
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m < 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+
+}
