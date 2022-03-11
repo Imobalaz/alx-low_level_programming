aadb3c84d9efc7b036a6f7b9e9a8babcb8590975
Imobalaz <imobalaz@gmail.com>
2022-03-11 06:52:00 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main main
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
