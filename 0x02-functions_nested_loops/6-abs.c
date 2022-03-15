e67462aab16af2a9067590c40f5babf4ba51f08a
Imobalaz <imobalaz@gmail.com>
2022-03-15 14:11:25 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ *
+ * @int : function variable
+ *
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	if (int < 0)
+		return (int * (-1));
+	else
+		return (int);
+}
