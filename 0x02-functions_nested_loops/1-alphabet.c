c8163bbc37bda85a4b1acadf3cdf70927339a6b5
Imobalaz <imobalaz@gmail.com>
2022-03-15 13:22:09 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 #include "main.h"
 
 /**
- * main - main block
+ * print_alphabet - main block
  *
  * Return: 0
  *
  */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 void print_alphabet(void)
 {
