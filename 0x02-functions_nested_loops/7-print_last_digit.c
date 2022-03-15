64335ad8be987c1d9b9100c8f5efaaa9f0b43c3c
Imobalaz <imobalaz@gmail.com>
2022-03-15 14:23:30 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - main function
+ *
+ * @n : function variable
+ *
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+	return (last_digit);
+}
