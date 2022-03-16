bcfbff612220c7ac6a31e0a788b1d056f90d85dc
Imobalaz <imobalaz@gmail.com>
2022-03-16 19:24:13 +0100

    fibonacci till 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * 
+ * Return: 0
+ * 
+ */
+
+int main(void)
+{
+    int i;
+    long j;
+    long k;
+
+    i = 0; 
+    j = 1;
+    k = 2;
+
+    while (i < 98)
+    {
+        if (i == 0)
+            printf("%li", j);
+        else if (i == 1)
+            printf("%li", k);
+        else 
+        {
+            k += j;
+            j = k - j;
+            printf("%li", k);
+        }
+
+        if (i != 97)
+            printf(", ");
+        i++;
+    }
+    printf("\n");
+    return (0);
+}
