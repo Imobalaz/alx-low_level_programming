3b042126b334610e5986336092d540a79a220c64
Imobalaz <imobalaz@gmail.com>
2022-03-15 19:06:33 -0700

    numbers up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,26 +14,23 @@ void print_to_98(int n)
 	
 	for (i = n; i <= 98; i++)
 	{
-		if (i % 2 == 0)
+		int m;
+
+		if (i < 0)
 		{
-			int m;
+			m = i * -1;
+			_putchar('-');
+		}
+		else
+			m = i;
+		if (m > 9)
+			_putchar('0'+ m / 10);
+		_putchar('0' + m % 10);
 
-			if (i < 0)
-			{
-				m = i * -1;
-				_putchar('-');
-			}
-			else
-				m = i;
-			if (m > 9)
-				_putchar('0'+ m / 10);
-			_putchar('0' + m % 10);
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	_putchar('\n');
