7cc0f2ade7a88cccd274aed03305a247e867e8ad
Imobalaz <imobalaz@gmail.com>
2022-03-16 14:22:28 -0700

    correcton

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,16 +18,7 @@ void print_to_98(int n)
 	{
 		for (; i <= 98; i++)
 		{
-			int m;
-
-			if (i < 0)
-			{
-				m = i * -1;
-				_putchar('-');
-			}
-			else
-				m = i;
-			printf("%i", m);
+			printf("%i", i);
 			if (i != 98)
 				printf(", ");
 		}
@@ -38,10 +29,7 @@ void print_to_98(int n)
 		{
 			printf("%i", i);
 			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf(", ");
 		}
 	}
 	_putchar('\n');
