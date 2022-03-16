b6de7456daa0e90b38ac5c3131fc32ffb887b8bd
Imobalaz <imobalaz@gmail.com>
2022-03-16 12:04:21 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,38 +3,38 @@
 
 /**
  * main - main block
- * 
+ *
  * Return: 0
- * 
+ *
  */
 
 int main(void)
 {
-    int i;
-    long j;
-    long k;
+	int i;
+	long j;
+	long k;
+	
+	i = 0; 
+	j = 1;
+	k = 2;
 
-    i = 0; 
-    j = 1;
-    k = 2;
-
-    while (i < 50)
-    {
-        if (i == 0)
-            printf("%li", j);
-        else if (i == 1)
-            printf("%li", k);
-        else 
-        {
-            k += j;
-            j = k - j;
-            printf("%li", k);
-        }
-
-        if (i != 49)
-            printf(", ");
-        i++;
-    }
-    printf("\n");
-    return (0);
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%li", j);
+		else if (i == 1)
+			printf("%li", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf("%li", k);
+		}
+		
+		if (i != 49)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
