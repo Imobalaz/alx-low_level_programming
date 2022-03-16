5dc31a3171aa8ef84edc089a378d151489cd3fef
Imobalaz <imobalaz@gmail.com>
2022-03-16 12:50:49 +0100

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+/**
+ * main - main block
+ * 
+ * Return: 0
+ * 
+ */
+
+int main(void)
+{
+    int i;
+    int sum;
+
+    for (i = 1; i < 1024; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    }
+
+    printf("%i\n", sum);
+    return (0);
+}
