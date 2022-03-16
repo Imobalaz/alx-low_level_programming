f19334953fa6801f5fca5dd79799b1ed0a76372d
Imobalaz <imobalaz@gmail.com>
2022-03-16 14:50:24 -0700

    Bigger times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_times_table - main function
+ *
+ * @n : function variable
+ *
+ * Return: 0
+ *
+ */
+
+void print_times_table(int n)
+{
+	if (n > 15 || n < 0)
+		return;
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < n + 1; i++)
+	{
+		for (j = 0; j < n + 1; j++)
+		{
+			k = i * j;
+
+			if (k < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + k);
+			}
+			else if (k > 9 && k < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + k / 10);
+				_putchar('0' + k % 10);
+			}
+			else
+			{
+				_putchar('0' + k / 100);
+				_putchar('0' + (k / 10) % 10);
+				_putchar('0' + k % 10);
+			}
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
