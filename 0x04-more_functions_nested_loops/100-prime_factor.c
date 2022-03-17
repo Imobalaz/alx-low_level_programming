062dc2aa3a76e0f637443b8ee106fc07807fc250
Imobalaz <imobalaz@gmail.com>
2022-03-17 12:24:34 -0700

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-long long largest_prime_factor(long long n);
-bool is_prime(long long i);
+long largest_prime_factor(long n);
+bool is_prime(long i);
 /**
  * main - main block
  * Return: 0
@@ -10,35 +10,39 @@ bool is_prime(long long i);
 
 int main(void)
 {
-    largest_prime_factor(612852475143);
+	largest_prime_factor(612852475143);
+	return (0);
 }
 
-long long largest_prime_factor(long long n)
+long largest_prime_factor(long n)
 {
-    long long largest;
-    for (long long i = 2; i < n; i++)
-    {
-        if (n % i == 0 && is_prime(i))
-        {
-            largest = i;
-        }
-    }
+	long largest;
+	long i;
 
-    if (largest == 0)
-        largest = n;
-
-    printf("%lli", largest);
-    return (largest);
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0 && is_prime(i))
+		{
+			largest = i;
+		}
+	}
+	
+	if (largest == 0)
+		largest = n;
+	printf("%li", largest);
+	return (largest);
 }
 
-bool is_prime(long long i)
+bool is_prime(long i)
 {
-    for(long long j = 2; j < i; j++)
-    {
-        if (i % j == 0)
-            return (false);
-    }
+	long j;
 
-    return (true);
+	for(j = 2; j < i; j++)
+	{
+		if (i % j == 0)
+			return (false);
+	}
+	
+	return (true);
 }
 
