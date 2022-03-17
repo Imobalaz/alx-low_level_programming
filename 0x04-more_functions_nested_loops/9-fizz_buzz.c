a47730ccd29343c45889877987671fed991acc07
Imobalaz <imobalaz@gmail.com>
2022-03-17 07:40:16 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,19 +7,21 @@
 
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i < 101; i++)
-    {
-        if ((i % 3 == 0) && (i % 5 == 0))
-            printf("FizzBuzz");
-        else if (i % 3 == 0)
-            printf("Fizz");
-        else if (i % 5 == 0)
-            printf("Buzz");
-        else
-            printf("%i", i);
-        if (i != 100)
-            printf(" ");
-    }
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i != 100)
+			printf(" ");
+	}
+
+	return (0);
 }
