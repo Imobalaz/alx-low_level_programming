98f8f8fce7dcf39d7f02015aa9b30491635b9805
Imobalaz <imobalaz@gmail.com>
2022-03-17 06:32:47 -0700

    something

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,17 @@
 
 /**
  * _isupper - checks for uppercase characters
+ *
  * @c : function variable
+ *
  * Return: 0
  */
 
 int _isupper(int c)
 {
 	if (c > 64 && c < 91)
-        {
-			_putchar('C');
-			return (1);
-		}
+	{
+		return (1);
+	}
 	return (0);
 }
