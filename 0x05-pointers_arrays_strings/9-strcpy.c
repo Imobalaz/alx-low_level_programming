8903680f66d8f01f92eb48263815bd76cb6cec2c
Imobalaz <imobalaz@gmail.com>
2022-03-21 06:04:12 -0700

    Copy STring

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcpy - function to copy string
+ *
+ * @dest : destination
+ *
+ * @src : source
+ *
+ * Return: pointer to the destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	len = 0;
+	while (*(src + len) != '\n')
+		len++;
+	for (i = 0; i < len + 1; i++)
+		*(dest + i) = *(src + i);
+	return (&dest);
+
+}
+
