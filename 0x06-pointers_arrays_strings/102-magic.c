3f13eb1c3f79427ba139a2f838875b9908769904
Imobalaz <imobalaz@gmail.com>
2022-03-24 15:17:01 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - main function
- *
- * Return: 0
- */
-
 int main(void)
 {
 	int n;
@@ -16,6 +10,7 @@ int main(void)
 	p = &n;
 
 	p[5] = 98;
+
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
