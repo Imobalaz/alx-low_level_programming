fcc47dc0582266fafe1d4d3dc1a3ebc82ad29201
Imobalaz <imobalaz@gmail.com>
2022-03-30 12:42:18 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,11 +13,12 @@ void _puts_recursion(char *s)
 	int len;
 
 	len = 0;
-	while (s[len])
-	{
-		_putchar(s[len]);
-		len++;
-	}
 
-	_putchar('\n');
+	if (s[len] == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[len]);
+	_puts_recursion(s + 1);
 }
