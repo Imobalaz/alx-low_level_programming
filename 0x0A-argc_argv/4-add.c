4750e63a6021e3364dd94587e05f536093ed0aa5
Imobalaz <imobalaz@gmail.com>
2022-04-01 12:17:16 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,24 +14,35 @@
 
 int main(int argc, char *argv[])
 {
-	int sum;
 	int i;
-
-	sum = 0;
+	unsigned int k, sum = 0;
+	char *e;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) && argv[i][0] != '0')
-				sum += atoi(argv[i]);
-			else
+			e = argv[i];
+
+			for (k = 0; k < strlen(e); k++)
 			{
-				printf("Error\n");
-				return (1);
+				if (e[k] < 48 || e[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+
+			sum += atoi(e);
+			e++;
 		}
+
+		printf("%d\n", sum);
 	}
-	printf("%i\n", sum);
+	else
+	{
+		printf("0\n");
+	}
+
 	return (0);
 }
