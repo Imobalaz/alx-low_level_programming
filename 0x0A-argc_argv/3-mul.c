db03fdd0af17b66b0a54c0a511e8969e4f6d4936
Imobalaz <imobalaz@gmail.com>
2022-04-01 10:11:40 -0700

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name
+ *
+ * @argc : number of arguments passed
+ *
+ * @argv : array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	product = atoi(argv[1]) * atoi(argv[2]);
+	printf("%i\n", product);
+	return (0);
+
+
+}
