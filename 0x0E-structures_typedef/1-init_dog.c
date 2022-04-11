e978ecdccdd4372030ab1f5d9633297a632e856c
Imobalaz <imobalaz@gmail.com>
2022-04-11 11:31:28 -0700

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,13 +10,16 @@
  * @age : age of the dog
  *
  * @owner: dog's owner
- * 
+ *
  * Return: null
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
