52b5d48d3f120a3fa7b33e0b462216ccf472c68c
Imobalaz <imobalaz@gmail.com>
2022-04-13 06:43:50 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name
+ *
+ * @name : name to be printed
+ *
+ * f : callback function that prints
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
